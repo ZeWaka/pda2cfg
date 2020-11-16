27c42df15ff3f5be0e04b284b10293f697232067
ZeWaka <zewakagamer@gmail.com>
2020-11-15 21:50:14 -0800

    Working with wrapped error stuff and command line arg filepath

---
+++ src/parser.rs
@@ -0,0 +1,53 @@
+use std::error::Error;
+
+use pest::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[grammar = "parser.pest"]
+pub struct CSVParser;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let file = CSVParser::parse(Rule::file, &contents)
+        .expect("unsuccessful parse") // unwrap the parse result
+        .next().unwrap(); // get and unwrap the `file` rule; never fails
+
+    let mut field_sum: f64 = 0.0;
+    let mut record_count: u64 = 0;
+
+    for record in file.into_inner() {
+        match record.as_rule() {
+            Rule::record => {
+                record_count += 1;
+
+                for field in record.into_inner() {
+                    field_sum += field.as_str().parse::<f64>().unwrap();
+                }
+            }
+            Rule::EOI => (),
+            _ => unreachable!(),
+        }
+    }
+
+    println!("Sum of fields: {}", field_sum);
+    println!("Number of records: {}", record_count);
+
+    Ok(())
+}
+pub struct Config {
+    pub filename: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+        
+        let filename = args[1].clone();
+
+        Ok(Config { filename })
+    }
+}
